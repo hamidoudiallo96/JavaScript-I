b20396459e21e6494fb84ded4776c2566a7e15cc
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-06-10 18:23:14 -0400

    MVP completed

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,27 +18,80 @@ const example = {
 }
 
 // Write your intern objects here:
+// 1,mmelloy0@psu.edu,Mitzi,F
+const Mitzi_bio = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+};
 
+// 2,kdiben1@tinypic.com,Kennan,M
+const Kennan_bio = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M",
+  hello: function(){
+    return`My name is ${this.name}! `
+  }
+};
+
+// 3,kmummery2@wikimedia.org,Keven,M
+const Keven_bio = {
+  "id": 3,
+  "name": "Keven",
+  "email": "Kmummery2@wikimedia",
+  "gender": "M",
+   // hello: function(){
+   //  return`My name is ${this.name} `
+   // }
+};
+
+// 4,gmartinson3@illinois.edu,Gannie,M
+const Gannie_bio = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartison3@illinois.edu",
+  "gender": "M"
+};
+// 5,adaine5@samsung.com,Antonietta,F
+const Antonietta_bio = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F",
+  math: function(a,b){
+    return a*b;
+  }
+};
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(Mitzi_bio.name)
 
 // Kennan's ID
+console.log(Kennan_bio.id)
 
 // Keven's email
+console.log(Keven_bio.email)
 
 // Gannie's name
+console.log(Gannie_bio.name)
 
 // Antonietta's Gender
+console.log(Antonietta_bio.gender)
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+console.log(Kennan_bio.hello())
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+console.log(Antonietta_bio.math(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
